834e2afa0107a878e5370df3b3a58bc474b49bb2
Ibraztech <Ibraztech2@gmail.com>
2025-01-22 02:44:59 -1100

    Calculator version Ready

--- simple_calculator.py
+++ simple_calculator.py
@@ -7,7 +7,6 @@ class Calculator:
         self.__operand__ = ""
         self.__result__ = None
         self.operandExpression = None
-        self.operator = None
         self.buttons = ["1", "2", "3",
                    "4", "5", "6",
                    "7", "8", "9",
@@ -16,7 +15,6 @@ class Calculator:
                    "=","Settings","Exit"]
         self.__rowValue__ = 1
         self.__columnValue__ = 1
-        self.__root__ = Tk()
 
 
 
@@ -51,12 +49,13 @@ class Calculator:
 
     def __buttonClick__(self,value):
 
-        self.__clickValue__ = value
-        if self.__clickValue__ not in "+-*/C" and (self.__clickValue__ != "Exit" and self.__clickValue__ != "Settings"):
-            self.__operand__  = self.__operand__+self.__clickValue__
+        self.__clickValue__ = str(value)
+        if self.__clickValue__ not in "+-*/C=" and (self.__clickValue__ != "Exit" and self.__clickValue__ != "Settings"):
+            self.__operand__  = self.__operand__+(self.__clickValue__)
             self.entry.delete(0,END)
             self.entry.insert(0,self.__operand__)
         elif self.__clickValue__ in "*/+-":
+            self.__operand__ = ""
             self.__operator__ = self.__clickValue__
             self.operand_1 = self.entry.get()
             self.entry.delete(0, END)
@@ -64,6 +63,9 @@ class Calculator:
         elif  self.__clickValue__ == "C":
             self.resetOperands()
             self.result = None
+        elif self.__clickValue__ == "=":
+            self.solve()
+            self.resetOperands()
 
     def displayPreviousResult(self,__value__):
         self.__value__ = __value__
@@ -75,40 +77,45 @@ class Calculator:
 
 
     def solve(self):
+        self.__operand__ = ""
         self.operand_2 = self.entry.get()
         self.entry.delete(0, END)
-        self.operandExpression  = f"{self.operand_1}{self.operator}{self.operand_2}"
+        self.operandExpression  = f"{self.operand_1}{self.__operator__}{self.operand_2}"
         self.__result__ = eval(self.operandExpression)
+        self.entry.insert(0,self.__result__)
 
     def resetOperands(self):
         self.operand_1 = None
         self.operand_2 = None
         self.__operand__ = ""
-        self.__result__ = None
         self.operandExpression = None
 
     def metaLabel(self):
+        self.centerMetaFrame = Label(self.__metaFrameHandler__,width=22,background="skyblue",height=2)
         self.getState = IntVar()
-        Label(self.__metaFrameHandler__,text="Ibraztech", fg="blue").grid(row=0,column=0)
-        Label(self.__metaFrameHandler__,text="  ", fg="blue").grid(row=0,column=1)
+
+
         if self.__result__ == None:
-            Checkbutton(self.__metaFrameHandler__, justify="center", command=lambda value=self.getState:self.displayPreviousResult(value),
+            self.__previousReult_CheckButton__ = Checkbutton(self.__metaFrameHandler__, justify="center", command=lambda value=self.getState:self.displayPreviousResult(value),
                         onvalue=True, offvalue=False,text="Previous Solution",state="disabled")#activebackground=,activeforeground=,background=)
         else:
             Checkbutton(self.__metaFrameHandler__, justify="center", command=lambda value=self.getState:self.displayPreviousResult(value),
                         onvalue=True, offvalue=False,text="Previous Solution",state="active")#activebackground=,activeforeground=,background=)
-        Label(self.__metaFrameHandler__,text="  ", fg="blue").grid(row=0,column=2)
-        Label(self.__metaFrameHandler__,text="Meta", fg="blue").grid(row=0,column=3)
+        Label(self.__metaFrameHandler__,text="Ibraztech", fg="blue",background="white").grid(row=0,column=0)
+
+        Label(self.__metaFrameHandler__,text="Meta", fg="blue",background="white").grid(row=0,column=2)
+        self.centerMetaFrame.grid(row=0,column=1)
 
     def settingsCallback(self):
         pass
 
 
     def __frameHandler__(self):
+        self.__root__ = Tk()
         self.__mainCalculatorWidget__ = Label(self.__root__,background="lightblue")
         self.entry = Entry(self.__mainCalculatorWidget__,justify="left",width=40)
         self.__buttonFrame__ = Label(self.__mainCalculatorWidget__, bg="skyblue")
-        self.__metaFrameHandler__ = Label(self.__mainCalculatorWidget__)
+        self.__metaFrameHandler__ = Label(self.__mainCalculatorWidget__,background="skyblue")
         self.__exitFrame__ = Label(self.__mainCalculatorWidget__,background="lightblue")
 
     def __gridHandlerAndMainloop__(self):
@@ -124,8 +131,8 @@ class Calculator:
 
     def activate(self):
         self.__frameHandler__()
-        self.__gridHandlerAndMainloop__()
         self.__displayButton__()
+        self.__gridHandlerAndMainloop__()
 
 
 
