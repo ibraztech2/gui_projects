775fd53acfea3c7aa122a14537877bc68fbe4a22
Ibraztech <Ibraztech2@gmail.com>
2025-01-22 01:45:23 -1100

    button frae not comming up

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,152 +1,138 @@
+
+
+class Calculator:
+    def __init__(self):
+        self.operand_1 = None
+        self.operand_2 = None
+        self.__operand__ = ""
+        self.__result__ = None
+        self.operandExpression = None
+        self.operator = None
+        self.buttons = ["1", "2", "3",
+                   "4", "5", "6",
+                   "7", "8", "9",
+                   "+", "0", "-",
+                   "/", "C", "*",
+                   "=","Settings","Exit"]
+        self.__rowValue__ = 1
+        self.__columnValue__ = 1
+        self.__root__ = Tk()
+
+
+
+
+    def __displayButton__(self):
+        self.metaLabel()
+        width = 9
+        height = 3
+        for button in self.buttons:
+            if button == "=":
+                Button(self.__buttonFrame__, text=button,  font=("Arial", 12),height=height, width=width * 2 + 1, justify="center", background="blue",
+                       fg="white", activeforeground="skyblue",
+                       command=lambda __value__=button: self.__buttonClick__(__value__)).grid(row=self.__rowValue__+1,
+                                                                                              column=1,columnspan = 2 )
+            elif button == "Exit":
+                Button(self.__exitFrame__, text="Exit", command=self.__root__.destroy, fg="BLUE", background="lightblue", justify="left",
+                       width=7, relief="groove", anchor="center", font=("Arial",10)).grid(row=self.__rowValue__, column=1, columnspan=2)
+            elif button == "Settings":
+                Button(self.__buttonFrame__, text=button, font=("Arial", 12), height=height, width=width, justify="center", background="blue",
+                       fg="white", activeforeground="skyblue",
+                       command=self.settingsCallback).grid(row=self.__rowValue__ +1, column=3)
+
+            else:
+                if self.__columnValue__ & 3 == 0:
+                    self.__rowValue__ += 1
+                    self.__columnValue__ = 1
+                Button(self.__buttonFrame__, font=("Arial", 12), text=button, height=3, width=width, justify="center", background="blue", fg="white", activeforeground="skyblue",
+                       command=lambda __value__ = button:self.__buttonClick__(__value__)).grid(row=self.__rowValue__,column=self.__columnValue__)
+                self.__columnValue__ +=1
+
+
+
+    def __buttonClick__(self,value):
+
+        self.__clickValue__ = value
+        if self.__clickValue__ not in "+-*/C" and (self.__clickValue__ != "Exit" and self.__clickValue__ != "Settings"):
+            self.__operand__  = self.__operand__+self.__clickValue__
+            self.entry.delete(0,END)
+            self.entry.insert(0,self.__operand__)
+        elif self.__clickValue__ in "*/+-":
+            self.__operator__ = self.__clickValue__
+            self.operand_1 = self.entry.get()
+            self.entry.delete(0, END)
+            self.entry.insert(0, self.__operator__)
+        elif  self.__clickValue__ == "C":
+            self.resetOperands()
+            self.result = None
+
+    def displayPreviousResult(self,__value__):
+        self.__value__ = __value__
+        if self.__value__:
+            self.entry.delete(0,END)
+            self.entry.insert(0,f"{self.operandExpression}")
+        else:
+            self.entry.delete(0,END)
+
+
+    def solve(self):
+        self.operand_2 = self.entry.get()
+        self.entry.delete(0, END)
+        self.operandExpression  = f"{self.operand_1}{self.operator}{self.operand_2}"
+        self.__result__ = eval(self.operandExpression)
+
+    def resetOperands(self):
+        self.operand_1 = None
+        self.operand_2 = None
+        self.__operand__ = ""
+        self.__result__ = None
+        self.operandExpression = None
+
+    def metaLabel(self):
+        self.getState = IntVar()
+        Label(self.__metaFrameHandler__,text="Ibraztech", fg="blue").grid(row=0,column=0)
+        Label(self.__metaFrameHandler__,text="  ", fg="blue").grid(row=0,column=1)
+        if self.__result__ == None:
+            Checkbutton(self.__metaFrameHandler__, justify="center", command=lambda value=self.getState:self.displayPreviousResult(value),
+                        onvalue=True, offvalue=False,text="Previous Solution",state="disabled")#activebackground=,activeforeground=,background=)
+        else:
+            Checkbutton(self.__metaFrameHandler__, justify="center", command=lambda value=self.getState:self.displayPreviousResult(value),
+                        onvalue=True, offvalue=False,text="Previous Solution",state="active")#activebackground=,activeforeground=,background=)
+        Label(self.__metaFrameHandler__,text="  ", fg="blue").grid(row=0,column=2)
+        Label(self.__metaFrameHandler__,text="Meta", fg="blue").grid(row=0,column=3)
+
+    def settingsCallback(self):
+        pass
+
+
+    def __frameHandler__(self):
+        self.__mainCalculatorWidget__ = Label(self.__root__,background="lightblue")
+        self.entry = Entry(self.__mainCalculatorWidget__,justify="left",width=40)
+        self.__buttonFrame__ = Label(self.__mainCalculatorWidget__, bg="skyblue")
+        self.__metaFrameHandler__ = Label(self.__mainCalculatorWidget__)
+        self.__exitFrame__ = Label(self.__mainCalculatorWidget__,background="lightblue")
+
+    def __gridHandlerAndMainloop__(self):
+        self.__mainCalculatorWidget__.grid(row=0,column=0)
+
+        #main Calculator widget child Class
+        self.entry.grid(row=0,column=0)
+        self.__metaFrameHandler__.grid(row=1, column=0)
+        self.__buttonFrame__.grid(row=2, column=0)
+        self.__exitFrame__.grid(row=3,column=0)
+
+        self.__root__.mainloop()
+
+    def activate(self):
+        self.__frameHandler__()
+        self.__gridHandlerAndMainloop__()
+        self.__displayButton__()
+
+
+
+
+
+
 from tkinter import *
 
-operand = ""
-operand_1 = None
-operand_2 = None
-operator_sign = None
-operand_expression = ""
-result_eval = None
-
-
-def button_click(value):
-    global operand, operator_sign, operand_2, operand_1, operand_expression
-    if value not in "+-/*=C":
-        entry.delete(0, END)
-        operand = operand + str(value)
-        entry.insert(0, operand)
-
-    if value in "*+/-":
-        operand_1 = entry.get()
-        operator_sign = value
-        entry.delete(0, END)
-        entry.insert(0, value)
-        operand = ""
-
-    elif value == "=":
-        operand_2 = entry.get()
-        operand_expression = f"{operand_1}{operator_sign}{operand_2}"
-        if not (operand_2 is None and operand_1 is None and operator_sign is None):
-            solve(operand_expression)
-        reset_operands()
-    elif value == "C":
-        entry.delete(0, END)
-        reset_operands()
-
-
-def reset_operands():
-    global operand_1, operand_2, operand, result_eval
-    operand_1 = ""
-    operand_2 = ""
-    operand = ""
-    result_eval = 0
-
-
-def solve(operand_expression):
-    global result_eval
-    try:
-        result_eval = eval(operand_expression)
-        entry.delete(0, END)
-        entry.insert(0, result_eval)
-        disp_previous_result_check()
-    except ZeroDivisionError as e:
-        entry.delete(0, END)
-        entry.insert(0, "Error")
-        reset_operands()
-        print(f"This Error is due to {e}")
-    finally:
-        reset_operands()
-
-
-def disp_previous_result(state_value):
-    if state_value:
-        entry.delete(0, END)
-        entry.insert(0, operand_expression)
-    elif not state_value:
-        entry.delete(0, END)
-        entry.insert(0, result_eval)
-
-
-def disp_previous_result_check():
-    previous_control_variable = IntVar()
-    if result_eval is None:
-        Checkbutton(meta_label_frame, text="Disp previous", background="white", fg="blue", anchor="center",
-                    state="disabled").grid(column=2, row=0)
-    else:
-        Checkbutton(meta_label_frame, text="Disp previous", background="blue", fg="white", anchor="center",
-                    state="active", activebackground="blue",
-                    command=lambda value=previous_control_variable: disp_previous_result(value), onvalue=True,
-                    offvalue=False, variable=previous_control_variable).grid(column=2, row=0)
-
-
-def meta_label():
-    f = " " * 12
-    disp_previous_result_check()
-    Label(meta_label_frame, text="Ibraztech", fg="BLUE", anchor="e").grid(column=0, row=0)
-    Label(meta_label_frame, text=f"{f}", fg="BLUE", background="skyblue", anchor="e").grid(column=1, row=0)
-    #Label(meta_label_frame, text=f"{f}", fg="BLUE", background="skyblue",anchor="e").grid(column=2, row=0)
-    Label(meta_label_frame, text=f"{f}", fg="BLUE", background="skyblue", anchor="e").grid(column=3, row=0)
-    Label(meta_label_frame, text="Meta", fg="BLUE", anchor="center", justify="left").grid(column=4, row=0)
-
-
-def clr_func():
-    reset_operands()
-    entry.delete(0, END)
-
-
-def settings():
-    pass
-
-
-root = Tk()
-root.title("Simple Calculator")
-pad_x = 150
-pad_y = 100
-main_calculator_widget = LabelFrame(root, background="skyblue")
-main_calculator_widget.grid(row=0, column=0)
-meta_label_frame = Label(main_calculator_widget, background="skyblue")
-meta_label_frame.grid(row=1, column=0)
-meta_label()
-
-entry = Entry(main_calculator_widget, borderwidth=5, border=2, width=39)
-entry.grid(row=0, column=0, columnspan=3, pady=10, padx=20)
-#Building a simple calculator
-buttons = ["1", "2", "3",
-           "4", "5", "6",
-           "7", "8", "9",
-           "+", "0", "-",
-           "/", "C", "*",
-           "="]
-button_frame = Label(main_calculator_widget, background="skyblue")
-button_frame.grid(row=2, column=0)
-row_value = 1
-column_value = 1
-for button in buttons:
-    if button == "=":
-        row_value += 1
-        column_value = 0
-        Button(button_frame, text=button, font=("Arial", 12), command=lambda value=button: button_click(value),
-               height=3,
-               width=19, bg="navyblue", fg="white", activebackground="lightblue", activeforeground="black",
-               justify="center").grid(
-            row=row_value, column=column_value, columnspan=3)
-    else:
-        if column_value & 3 == 0:
-            row_value += 1
-            column_value = 1
-
-        Button(button_frame, text=button, font=("Arial", 12), command=lambda value=button: button_click(value),
-               height=3,
-               width=9, bg="navyblue", fg="white", activebackground="lightblue", activeforeground="black").grid(
-            row=row_value, column=column_value)
-        column_value += 1
-
-Button(button_frame, command=settings, text="Settings", background="navyblue", height=2,
-       foreground="white", anchor="center", font=("Arial", 16)).grid(row=6, column=3)
-Button(main_calculator_widget, text="Exit", command=root.destroy, fg="BLUE", background="lightblue", justify="left",
-       width=7, relief="groove", anchor="center", font="Arial,10").grid()
-"""
-Label(meta_label_frame, text="Ibraztech", fg="BLUE", anchor="e").grid(column=0, row=0)
-Label(meta_label_frame, text=" " * 50, background="skyblue", fg="skyblue", anchor="e").grid(column=1, row=0)
-Label(meta_label_frame, text="Meta", fg="BLUE", anchor="center", justify="left").grid(column=2, row=0)"""
-
-root.mainloop()
+calculator = Calculator()
+calculator.activate()
