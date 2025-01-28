a61c2c803453258f1257f2952f9d6322531b467a
Ibraztech <ibraztech2@gmail.com>
2025-01-28 02:31:48 -1100

    Version 1.00

--- todo_list.py
+++ todo_list.py
@@ -1,23 +1,38 @@
-def create_task():
-    date_checked = ""
-    get_completion_date_input = IntVar()
-    task_status = False
-    date_created = datetime_stamp
-    task_list.append(f"{task_entry.get()}~{task_status}~{date_created}~{date_checked}")
-    save_task()
-    display_task()
+def add_date():
+    def spinbox_window_terminator():
+        date_created = datetime_stamp
+        completion_date = f"{get_completion_year.get()}-{get_completion_month.get()}-{get_completion_day.get()}"
+        task_list.append(f"{task_entry.get()}~{task_status}~{date_created}~{completion_date}")
+        save_task()
+        display_task()
+        spin_box_window.destroy()
 
+    get_completion_day = IntVar()
+    get_completion_month = IntVar()
+    get_completion_year = IntVar()
+    task_status = False
+    if task_entry.get():
+        spin_box_window = Toplevel(todo_list_frame_1)
+        Spinbox(spin_box_window, from_=1, increment=1, to=30, textvariable=get_completion_day).grid()
+        Spinbox(spin_box_window, from_=1, increment=1, to=12, textvariable=get_completion_month).grid()
+        Spinbox(spin_box_window, from_=datetime_stamp.year, increment=1, to=datetime_stamp.year + 50,
+                textvariable=get_completion_year).grid()
+        Button(spin_box_window, text="Done", command=spinbox_window_terminator).grid()
+
+        spin_box_window.grid()
+    else:
+        messagebox.showerror("Date Error","Input task before adding date")
 
 
 def delete_task():
     global delete_task_call
-    delete_task_call=True
-    delete_response = messagebox.askyesno("Delete","press okay to delete task")
+    delete_task_call = True
+    delete_response = messagebox.askyesno("Delete", "press okay to delete task")
     if delete_response:
         if not task_entry.get():
             if task_listbox.curselection():
                 get_current_selection = task_listbox.curselection()
-                get_current_selection = len(task_list)- int(get_current_selection[0]) -1
+                get_current_selection = len(task_list) - int(get_current_selection[0]) - 1
                 task_list.pop(get_current_selection)
 
         else:
@@ -31,44 +46,43 @@ def display_task():
     task_listbox.delete(0, END)
 
     for task in task_list:
-        current_task,status,date_created,date_checked = task.split("~")
-        if date_checked :
+        current_task, status, date_created, date_checked = task.split("~")
+        if date_checked:
             if status == "True":
-                task_listbox.insert(0,f"Task:{current_task} Status: Completed ")
+                task_listbox.insert(0, f"Task:{current_task} Status: Completed ")
             else:
-                task_listbox.insert(0,f"Task:{current_task} Status: Not Completed")
+                task_listbox.insert(0, f"Task:{current_task} Status: Not Completed")
 
         else:
-            task_listbox.insert(0,f"Task:{current_task} Status: Not Completed")
-
+            task_listbox.insert(0, f"Task:{current_task} Status: Not Completed")
 
 
 def check_task():
     global check_task_call
-    check_task_call=True
+    check_task_call = True
     get_current_selection = task_listbox.curselection()
     if get_current_selection:
         get_current_selection = len(task_list) - int(get_current_selection[0]) - 1
         task = task_list[get_current_selection]
-        current_task, status, date_created, date_checked = task.split("~")
+        current_task, status, date_created, completion_date = task.split("~")
         new_status = "True" if status == "False" else "False"
-        check_datetime_stamp = datetime_stamp
-        task_list[get_current_selection] =f"{current_task}~{new_status}~{date_created}~{check_datetime_stamp}"
+        task_list[get_current_selection] = f"{current_task}~{new_status}~{date_created}~{completion_date}"
         save_task()
         display_task()
     else:
-        messagebox.showerror("Check Error","Please select a task to check/Uncheck")
+        messagebox.showerror("Check Error", "Please select a task to check/Uncheck")
 
     check_task_call = False
+
 
 def save_task():
     if task_entry.get() or delete_task_call or check_task_call:
         with open(text_file_path, "w") as files:
             for task in task_list:
-                current_task, status, date_created, date_checked = task.split("~")
+                current_task, status, date_created, completion_date = task.split("~")
 
                 if task:
-                    files.write(f"{current_task}~{status}~{date_created}~{date_checked}\n")
+                    files.write(f"{current_task}~{status}~{date_created}~{completion_date}\n")
 
         display_task()
 
@@ -88,21 +102,24 @@ def load_task():
     if path.exists(text_file_path):
         task_file_to_list()
         display_task()
+
+
 def task_details():
     get_current_selection = task_listbox.curselection()
     if get_current_selection:
         get_task_details_val = task_detail_contl_var.get()
         get_current_selection = len(task_list) - int(get_current_selection[0]) - 1
         if get_task_details_val:
-            current_task, status, date_created, date_checked = task_list[get_current_selection].split("~")
+            current_task, status, date_created, completion_date = task_list[get_current_selection].split("~")
+            year, month, date = completion_date.split("-")
             print(task_list[get_current_selection])
-            messagebox.showinfo(f"{current_task} Details",f"Task: {current_task}\n"
-                                                          f"Status: {"completed" if status == "True" else "Not Completed"}\n"
-                                                          f"Date Created: {date_created}\n"
-                                                          f"Date checked: {date_checked if status == "True" else "Not Checked yet"}")
+            print(completion_date)
+            messagebox.showinfo(f"{current_task} Details", f"Task: {current_task}\n"
+                                                           f"Status: {"completed" if status == "True" else "Not Completed"}\n"
+                                                           f"Date Created: {date_created}\n"
+                                                           f" Due Date: {completion_date}\n")
     else:
         messagebox.showerror("Show Details Error", "Please select task to show its details")
-
 
 
 def exit_todo():
@@ -124,7 +141,6 @@ def exit_todo():
         todo_list_main_window.destroy()
 
 
-
 from tkinter import messagebox
 from tkinter import *
 from time import time
@@ -137,7 +153,6 @@ todo_list_main_window = Tk()
 
 time_stamp = time()
 datetime_stamp = datetime.fromtimestamp(time_stamp)
-
 
 task_list = []
 delete_task_call = False
@@ -156,9 +171,9 @@ task_listbox = Listbox(todo_list_frame_1, font=font, width=list_width)
 # Load Previous Tasks
 load_task()
 
-check_task_button = Button(button_frame, anchor="center", justify="center",height=button_height, width=button_width,
-                                text="check/uncheck task", command=check_task)
-create_button = Button(button_frame, text="Create", height=button_height, width=button_width, command=create_task,
+check_task_button = Button(button_frame, anchor="center", justify="center", height=button_height, width=button_width,
+                           text="check/uncheck task", command=check_task)
+create_button = Button(button_frame, text="Add Date", height=button_height, width=button_width, command=add_date,
                        justify="center")
 delete_button = Button(button_frame, text="remove task", height=button_height, width=button_width, command=delete_task,
                        justify="center")
@@ -166,16 +181,18 @@ save_button = Button(button_frame, text="Save task", height=button_height, width
                      justify="center")
 exit_button = Button(button_frame, text="Exit", height=button_height, width=button_width, command=exit_todo,
                      justify="center")
-task_details_button = Checkbutton(todo_list_frame_1, anchor="center", justify="center",height=button_height, width=button_width,
-                                text="Show Task Details", command=task_details,onvalue=True,offvalue=False,variable=task_detail_contl_var,state="active")
+task_details_button = Checkbutton(todo_list_frame_1, anchor="center", justify="center", height=button_height,
+                                  width=button_width,
+                                  text="Show Task Details", command=task_details, onvalue=True, offvalue=False,
+                                  variable=task_detail_contl_var, state="active")
 
 #Frame subwidget Grid center
 pady, padx = 10, 20
 todo_list_title_label.grid(row=0, column=0, columnspan=3)
-task_entry.grid(row=1, column=0, padx=padx, pady=pady, ipady=pady + 5, ipadx=padx,columnspan=3)
+task_entry.grid(row=1, column=0, padx=padx, pady=pady, ipady=pady + 5, ipadx=padx, columnspan=3)
 button_frame.grid(row=2, column=1, columnspan=3, pady=pady, padx=padx)
 task_listbox.grid(row=3, column=1, columnspan=3, pady=pady, padx=padx, sticky="nsew")
-task_details_button.grid(row=4,column=0,columnspan=3)
+task_details_button.grid(row=4, column=0, columnspan=3)
 
 create_button.grid(row=0, column=1)
 check_task_button.grid(row=1, column=0)
@@ -183,9 +200,7 @@ exit_button.grid(row=1, column=1)
 save_button.grid(row=1, column=2)
 delete_button.grid(row=2, column=1)
 
-
 # MainFrame Grid
 todo_list_frame_1.grid(row=0, column=0)
-
 #todo_list_main_window.geometry("400x400")
 todo_list_main_window.mainloop()
