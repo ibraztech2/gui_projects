1baa29315904de9bcf8136c20b9ca5d946547f09
Ibraztech <ibraztech2@gmail.com>
2025-02-15 10:18:51 -1100

    finish oop_version

--- app_tester.py
+++ app_tester.py
@@ -1,7 +1,12 @@
-from simple_calculator import calculator
+#from simple_calculator import calculator
+from todo_list import ToDoList
 
-
+"""
 #calculator = Calculator()
 calculator.calculatorGeometry("calculator",518,281)
 calculator.buttonParameters(button_afg="lightgreen",frame_bg="white")
 calculator.activate()
+"""
+
+
+todo_list = ToDoList()
--- todo_list.py
+++ todo_list.py
@@ -1,206 +1,217 @@
-def add_date():
-    def spinbox_window_terminator():
-        date_created = datetime_stamp
-        completion_date = f"{get_completion_year.get()}-{get_completion_month.get()}-{get_completion_day.get()}"
-        task_list.append(f"{task_entry.get()}~{task_status}~{date_created}~{completion_date}")
-        save_task()
-        display_task()
-        spin_box_window.destroy()
-
-    get_completion_day = IntVar()
-    get_completion_month = IntVar()
-    get_completion_year = IntVar()
-    task_status = False
-    if task_entry.get():
-        spin_box_window = Toplevel(todo_list_frame_1)
-        Spinbox(spin_box_window, from_=1, increment=1, to=30, textvariable=get_completion_day).grid()
-        Spinbox(spin_box_window, from_=1, increment=1, to=12, textvariable=get_completion_month).grid()
-        Spinbox(spin_box_window, from_=datetime_stamp.year, increment=1, to=datetime_stamp.year + 50,
-                textvariable=get_completion_year).grid()
-        Button(spin_box_window, text="Done", command=spinbox_window_terminator).grid()
-
-        spin_box_window.grid()
-    else:
-        messagebox.showerror("Date Error","Input task before adding date")
-
-
-def delete_task():
-    global delete_task_call
-    delete_task_call = True
-    delete_response = messagebox.askyesno("Delete", "press okay to delete task")
-    if delete_response:
-        if not task_entry.get():
-            if task_listbox.curselection():
-                get_current_selection = task_listbox.curselection()
-                get_current_selection = len(task_list) - int(get_current_selection[0]) - 1
-                task_list.pop(get_current_selection)
-
-        else:
-            messagebox.showerror("Error", "Task input entry contain unsaved tasks")
-        save_task()
-        display_task()
-        delete_task_call = False
-
-
-def display_task():
-    task_listbox.delete(0, END)
-
-    for task in task_list:
-        current_task, status, date_created, date_checked = task.split("~")
-        if date_checked:
-            if status == "True":
-                task_listbox.insert(0, f"Task:{current_task} Status: Completed ")
-            else:
-                task_listbox.insert(0, f"Task:{current_task} Status: Not Completed")
-
-        else:
-            task_listbox.insert(0, f"Task:{current_task} Status: Not Completed")
-
-
-def check_task():
-    global check_task_call
-    check_task_call = True
-    get_current_selection = task_listbox.curselection()
-    if get_current_selection:
-        get_current_selection = len(task_list) - int(get_current_selection[0]) - 1
-        task = task_list[get_current_selection]
-        current_task, status, date_created, completion_date = task.split("~")
-        new_status = "True" if status == "False" else "False"
-        task_list[get_current_selection] = f"{current_task}~{new_status}~{date_created}~{completion_date}"
-        save_task()
-        display_task()
-    else:
-        messagebox.showerror("Check Error", "Please select a task to check/Uncheck")
-
-    check_task_call = False
-
-
-def save_task():
-    if task_entry.get() or delete_task_call or check_task_call:
-        with open(text_file_path, "w") as files:
-            for task in task_list:
-                current_task, status, date_created, completion_date = task.split("~")
-
-                if task:
-                    files.write(f"{current_task}~{status}~{date_created}~{completion_date}\n")
-
-        display_task()
-
-    else:
-        messagebox.showerror("Error", f"Error, input task !!! ")
-    task_entry.delete(0, END)
-
-
-def task_file_to_list():
-    if path.exists(text_file_path):
-        with open(text_file_path, "r") as tasks:
-            for task in tasks:
-                task_list.append(task.strip())
-
-
-def load_task():
-    if path.exists(text_file_path):
-        task_file_to_list()
-        display_task()
-
-
-def task_details():
-    get_current_selection = task_listbox.curselection()
-    if get_current_selection:
-        get_task_details_val = task_detail_contl_var.get()
-        get_current_selection = len(task_list) - int(get_current_selection[0]) - 1
-        if get_task_details_val:
-            current_task, status, date_created, completion_date = task_list[get_current_selection].split("~")
-            year, month, date = completion_date.split("-")
-            print(task_list[get_current_selection])
-            print(completion_date)
-            messagebox.showinfo(f"{current_task} Details", f"Task: {current_task}\n"
-                                                           f"Status: {"completed" if status == "True" else "Not Completed"}\n"
-                                                           f"Date Created: {date_created}\n"
-                                                           f" Due Date: {completion_date}\n")
-    else:
-        messagebox.showerror("Show Details Error", "Please select task to show its details")
-
-
-def exit_todo():
-    exit_response = None
-    if not task_entry.get():
-        exit_response = messagebox.askokcancel("Exit", "Press okay to Exit App")
-    else:
-        exit_response = messagebox.askyesno("Unsaved Tasks", "You have unsaved tasks, press Yes continue")
-        print(exit_response)
-        if exit_response:
-            exit_response = messagebox.askyesno("Save before exit", "Press yes to save before Exit")
-            if exit_response:
-                save_task()
-                messagebox.askokcancel("Saved", "Saved Successfully")
-            else:
-                exit_response = True
-
-    if exit_response:
-        todo_list_main_window.destroy()
-
-
-from tkinter import messagebox
+import time
 from tkinter import *
+from datetime import *
 from time import time
-from datetime import datetime
 from os import path
+from tkinter import messagebox
 
-from tkinter import *
 
-todo_list_main_window = Tk()
 
-time_stamp = time()
-datetime_stamp = datetime.fromtimestamp(time_stamp)
 
-task_list = []
-delete_task_call = False
-check_task_call = False
-font, button_width, button_height = ("Arial", 15), 15, 2
-text_file_path = "task_data.txt"
-list_width = 60
-task_detail_contl_var = BooleanVar()
+class ToDoList:
+    def __init__(self, master=Tk(), button_height=2,button_width = 15,entry_width = 60):
+        self.master,self.button_height,self.button_width,self.entry_width = master,button_height,button_width,entry_width
+        self.taskList = []
+        self.filePath = "task_data.txt"
+        self.font = ("Arial",15)
+        self.getShowProperties = BooleanVar()
 
-todo_list_frame_1 = Frame(todo_list_main_window)
-button_frame = Frame(todo_list_frame_1)
-todo_list_title_label = Label(todo_list_frame_1, text="ToDo list", justify="center", font=font)
-task_entry = Entry(todo_list_frame_1, font=font, justify="left", width=list_width)
-task_listbox = Listbox(todo_list_frame_1, font=font, width=list_width)
 
-# Load Previous Tasks
-load_task()
+        self.activateToDoList()
 
-check_task_button = Button(button_frame, anchor="center", justify="center", height=button_height, width=button_width,
-                           text="check/uncheck task", command=check_task)
-create_button = Button(button_frame, text="Add Date", height=button_height, width=button_width, command=add_date,
-                       justify="center")
-delete_button = Button(button_frame, text="remove task", height=button_height, width=button_width, command=delete_task,
-                       justify="center")
-save_button = Button(button_frame, text="Save task", height=button_height, width=button_width, command=save_task,
-                     justify="center")
-exit_button = Button(button_frame, text="Exit", height=button_height, width=button_width, command=exit_todo,
-                     justify="center")
-task_details_button = Checkbutton(todo_list_frame_1, anchor="center", justify="center", height=button_height,
-                                  width=button_width,
-                                  text="Show Task Details", command=task_details, onvalue=True, offvalue=False,
-                                  variable=task_detail_contl_var, state="active")
+    def load(self):
+        if path.exists(self.filePath):
+            with open(self.filePath,"r") as files:
+                for task in files:
+                    if "~" in task:
+                        self.taskList.append(task.strip())
+                self.display()
 
-#Frame subwidget Grid center
-pady, padx = 10, 20
-todo_list_title_label.grid(row=0, column=0, columnspan=3)
-task_entry.grid(row=1, column=0, padx=padx, pady=pady, ipady=pady + 5, ipadx=padx, columnspan=3)
-button_frame.grid(row=2, column=1, columnspan=3, pady=pady, padx=padx)
-task_listbox.grid(row=3, column=1, columnspan=3, pady=pady, padx=padx, sticky="nsew")
-task_details_button.grid(row=4, column=0, columnspan=3)
 
-create_button.grid(row=0, column=1)
-check_task_button.grid(row=1, column=0)
-exit_button.grid(row=1, column=1)
-save_button.grid(row=1, column=2)
-delete_button.grid(row=2, column=1)
+    def display(self):
+        self.taskListBox.delete(0, END)
 
-# MainFrame Grid
-todo_list_frame_1.grid(row=0, column=0)
-#todo_list_main_window.geometry("400x400")
-todo_list_main_window.mainloop()
+        for task in self.taskList:
+            current_task, status, date_created, completion_date = task.split("~")
+            if completion_date:
+                if status == "True":
+                    self.taskListBox.insert(0, f"Task:{current_task} Status: Completed ")
+                else:
+                    self.taskListBox.insert(0, f"Task:{current_task} Status: Not Completed  To be completed on {completion_date}")
+
+            else:
+                self.taskListBox.insert(0, f"Task:{current_task} Status: Not Completed  To be completed on {completion_date}")
+
+
+    def createTask(self):
+        self.getTask = self.taskEntry.get()
+        self.status = False
+
+        self.dateCreated = datetime.fromtimestamp(time())
+        if self.getTask:
+            completion_date= f"{self.getCurrentYear.get()}-{self.getCurrentMonth.get()}-{self.getCurrentDay.get()}"
+            self.taskList.append(f"{self.getTask}~{self.status}~{self.dateCreated}~{completion_date}")
+        self.saveTask()
+        self.display()
+        self.taskEntry.delete(0,END)
+
+    def addDate(self):
+        self.getCurrentDay = IntVar()
+        self.getCurrentMonth = IntVar()
+        self.getCurrentYear = IntVar()
+        self.currentYear = datetime.fromtimestamp(time()).year
+        self.currentMonth =  1 if self.getCurrentYear.get() > self.currentYear  else datetime.fromtimestamp(time()).month
+        self.currentDay =  1 if self.getCurrentMonth.get() > self.currentMonth  else datetime.fromtimestamp(time()).day
+
+        if self.taskEntry.get():
+            self.topLevel_DateWindow  = Toplevel(self.todo_list_frame_1)
+            self.topLevel_DateWindow.title("Date of Completion")
+            displayday = Label(self.topLevel_DateWindow,text="Day")
+            displaymonth = Label(self.topLevel_DateWindow,text="Month")
+            displayyear = Label(self.topLevel_DateWindow,text="Year")
+            self.getDay  = Spinbox(self.topLevel_DateWindow, from_=self.currentDay, to=30, increment=1, textvariable=self.getCurrentDay,width=10,font=self.font)
+            self.getMonth  = Spinbox(self.topLevel_DateWindow, from_=self.currentMonth, to=12, increment=1, textvariable=self.getCurrentMonth,width=10,font=self.font)
+            self.getYear  = Spinbox(self.topLevel_DateWindow, from_=self.currentYear, to=self.currentYear + 50, increment=1, textvariable=self.getCurrentYear,width=10,font=self.font)
+            self.doneButton = Button(self.topLevel_DateWindow, command=self.topLevel_DateWindow.destroy, text="Save Date", padx=5, pady=5)
+
+            # topLevel_DateWindow child widget
+            displayday.grid(row=0,column=0)
+            displaymonth.grid(row=0,column=1)
+            displayyear.grid(row=0,column=2)
+            self.getDay.grid(row=1,column=0)
+            self.getMonth.grid(row=1,column=1)
+            self.getYear.grid(row=1,column=2)
+            self.doneButton.grid(row=2,column=0,columnspan=3)
+        else:
+            messagebox.showerror("Error","Input task First !!!")
+
+
+    def saveTask(self):
+        if path.exists(self.filePath):
+            if self.taskEntry.get() or self.delete_check:
+                with open(self.filePath,"w") as files:
+                    for task in self.taskList:
+                        if "~" in  task:
+                            current_task, status, date_created, completion_date = task.split("~")
+                            files.write(f"{current_task}~{status}~{date_created}~{completion_date}\n")
+
+        else:
+            messagebox.showerror("Error","Error opening file")
+
+
+
+    def deleteTask(self):
+        get_current_selection = self.taskListBox.curselection()
+        get_current_selection = len(self.taskList) - 1 - get_current_selection[0]
+        self.taskList.pop(get_current_selection)
+        self.delete_check = True
+        self.saveTask()
+        self.display()
+        self.delete_check = False
+
+
+    def checkTask(self):
+        get_current_selection = self.taskListBox.curselection()
+        get_current_selection =len(self.taskList) - 1 - get_current_selection[0]
+        task,status,date_created,completion_date  = self.taskList[get_current_selection].split("~")
+        status = "False" if status == "True" else "True"
+        self.taskList[get_current_selection] = f"{task}~{status}~{date_created}~{completion_date}"
+        self.saveTask()
+        self.display()
+
+    def showDetails(self):
+        get_current_selection = self.taskListBox.curselection()
+        if get_current_selection:
+            get_current_selection = get_current_selection[0]
+            if self.getShowProperties.get():
+                task, status, date_created, completion_date = self.taskList[len(self.taskList)-1-get_current_selection].split("~")
+                messagebox.showinfo(f"{task} Details,Task: {task}\n "
+                                    f"Status: {"completed" if status == "True" else "Not Completed"}\n"
+                                    f"Date Created: {date_created}\n"
+                                    f"Due Date: {completion_date}")
+
+        else:
+            messagebox.showerror("Error","Please Select a Task ")
+
+    def exitToDo(self):
+        exit_response = None
+        quit_unsaved_exit_response = False
+        if not self.taskEntry.get():
+            exit_response = messagebox.askokcancel("Exit", "Press okay to Exit App")
+        else:
+            exit_response = messagebox.askyesnocancel("Unsaved Tasks", "You have unsaved tasks, press Yes continue")
+            print(exit_response)
+            if exit_response:
+                exit_response = messagebox.askyesno("Save before exit", "Press yes to save before Exit")
+                if exit_response:
+                    self.saveTask()
+                    messagebox.askokcancel("Saved", "Saved Successfully")
+                else:
+                    exit_response = True
+            elif not exit_response:
+                quit_unsaved_exit_response = True
+            elif exit_response is None:
+                pass
+
+
+        if exit_response or quit_unsaved_exit_response:
+            self.master.destroy()
+
+    #Gui Management start from here
+
+
+    def geometryHandler(self):
+        self.todo_list_frame_1 = Frame(self.master)
+
+    def initializeWidget(self):
+        self.title = Label(self.todo_list_frame_1,text="ToDo List",font=("Arial",30),justify="center")
+        self.buttonFrame = Frame(self.todo_list_frame_1)
+        self.taskEntry = Entry(self.todo_list_frame_1,width=self.entry_width,font=self.font)
+        self.taskListBox = Listbox(self.todo_list_frame_1,width=self.entry_width,font=self.font)
+        self.detailsFrame = Frame(self.todo_list_frame_1)
+
+        self.addDateButton = Button(self.buttonFrame, width=self.button_width, height=self.button_height, text="Add Date", justify="center", command=self.addDate)
+        self.deleteTaskButton = Button(self.buttonFrame, width=self.button_width, height=self.button_height, text="Delete", justify="center", command=self.deleteTask)
+        self.saveTaskButton = Button(self.buttonFrame, width=self.button_width, height=self.button_height, text="Save", justify="center", command=self.createTask)
+        self.checkTaskButton = Button(self.buttonFrame, width=self.button_width, height=self.button_height, text="Check/Uncheck", justify="center", command=self.checkTask)
+        self.exitToDoButton = Button(self.buttonFrame, width=self.button_width, height=self.button_height, text="Exit", justify="center", command=self.exitToDo)
+
+        self.detailsButton = Checkbutton(self.detailsFrame,text="show Properties",command=self.showDetails,variable=self.getShowProperties,onvalue=True,offvalue=False)
+
+
+
+
+
+
+    def grid_handlers(self):
+        # Todo child Widget
+        self.title.grid(row=0,column=0,pady=20,padx=20)
+        self.taskEntry.grid(row=1,column=0,pady=5,padx=20)
+        self.buttonFrame.grid(row=2,column=0,pady=20,padx=20)
+        self.taskListBox.grid(row=3,column=0,pady=5,padx=5)
+        self.detailsFrame.grid(row=4,column=0)
+
+        # Button Frame Child widget
+        self.addDateButton.grid(row=0, column=1)
+        self.checkTaskButton.grid(row=1, column=0)
+        self.exitToDoButton.grid(row=1, column=1)
+        self.saveTaskButton.grid(row=1, column=2)
+        self.deleteTaskButton.grid(row=2, column=1)
+
+        #details frame child widget
+        self.detailsButton.grid(row=0,column=0,columnspan=3)
+
+
+
+        self.todo_list_frame_1.grid(row=0, column=0)
+        self.master.mainloop()
+
+
+    #This Method manage all the default method  of the app
+    def activateToDoList(self):
+        self.geometryHandler()
+        self.initializeWidget()
+        self.load()
+        self.grid_handlers()
+
+
